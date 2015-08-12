959fbb7d9258590c6b2a3f241a1af12cc0a61158
Ryan N. Norris <thenameisnigel@users.noreply.github.com>
2015-08-12 16:43:13 -0400

    Organize some things.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,7 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/alel04/BoardConfigVendor.mk
 
+# Some things.
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
@@ -15,17 +16,19 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := alel04
 
+# Kernel Stuff
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := device/huawei/alel04/kernel
 
+
+# Block Size Stuff
 # fix this up by examining /proc/mtd on a running device
+# I commented this out because it was aggrevating me.
 # BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
 # BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-TARGET_PREBUILT_KERNEL := device/huawei/alel04/kernel
-
 BOARD_HAS_NO_SELECT_BUTTON := true
