4537126a0486cecce4d0d8277fec6b6b1c462442
Ryan N. Norris <thenameisnigel@users.noreply.github.com>
2015-08-12 18:03:15 -0400

    Some stuff to be fixed up later.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,18 +3,20 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/huawei/alel04/BoardConfigVendor.mk
 
-# Some things.
+# Architecture
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm8610
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := cortex-a7
 TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-
 TARGET_BOOTLOADER_BOARD_NAME := alel04
+
+# Power
+TARGET_POWERHAL_VARIANT := cm
 
 # Kernel Stuff
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
@@ -24,6 +26,10 @@ BOARD_KERNEL_PAGESIZE := 2048
 # TARGET_KERNEL_SOURCE := kernel/huawei/msm8610
 TARGET_PREBUILT_KERNEL := device/huawei/alel04/kernel
 
+# Vold
+# BOARD_VOLD_MAX_PARTITIONS := 25
+# TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+# TARGET_USE_CUSTOM_LUN_FILE_PATH := /lun%d/file
 
 # Block Size Stuff
 # fix this up by examining /proc/mtd on a running device
