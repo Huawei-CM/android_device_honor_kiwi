59ce92ef30368061e2e1edaef493a33af14d9ae3
Ryan N. Norris <thenameisnigel@users.noreply.github.com>
2015-08-12 16:02:18 -0400

    sigh

--- device_alel04.mk
+++ device_alel04.mk
@@ -15,7 +15,9 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_PATH)/recovery/etc/fstab.qcom:recovery/root/fstab.qcom \
+    $(LOCAL_PATH)/recovery/etc/init.recovery.qcom.rc:root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/kernel:kernel
 
 $(call inherit-product, build/target/product/full.mk)
 
