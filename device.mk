5a80e39bc81af533a67df80b241121d32bedda98
Shane Passmore <h8rift@gmail.com>
2015-08-16 21:31:43 -0500

    alel04: add media configs
    Change-Id: Ie1eda550645897e6ac32c099c77df8fb44d01521

--- device.mk
+++ device.mk
@@ -48,6 +48,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH))/overlay
 
 PRODUCT_COPY_FILES += \
