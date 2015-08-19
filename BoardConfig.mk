1f84f499a705f56119db0db0dc693cad21445634
Shane Passmore <h8rift@gmail.com>
2015-08-18 19:49:21 -0500

    alel04: re-arrange BoardConfig and add some correct values
    Change-Id: I0de66cfcf22a8c1ad211902728d8efe2e87db2c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,19 +1,36 @@
-USE_CAMERA_STUB := true
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# inherit from the proprietary version
--include vendor/huawei/alel04/BoardConfigVendor.mk
+LOCAL_PATH := device/huawei/alel04
+
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := msm8610
+TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_MEMCPY_BASE_OPT_DISABLE := true
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_BOARD_PLATFORM := msm8610
 TARGET_BOOTLOADER_BOARD_NAME := alel04
+BOARD_USES_QCOM_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 
 # Power
 TARGET_POWERHAL_VARIANT := cm
@@ -24,18 +41,22 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := cm_alel04_defconfig
 TARGET_KERNEL_SOURCE := kernel/huawei/msm8610
-# TARGET_PREBUILT_KERNEL := device/huawei/alel04/kernel
 
-# Vold
-# BOARD_VOLD_MAX_PARTITIONS := 25
-# TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-# TARGET_USE_CUSTOM_LUN_FILE_PATH := /lun%d/file
-
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_CACHEIMAGE_PARTITION_SIZE := 201326592
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1879048192
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 11620301824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12803095552
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BASE_KERNEL_PAGESIZE * 64)
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+
+
+# Vold
+BOARD_VOLD_MAX_PARTITIONS := 25
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# inherit from the proprietary version
+-include vendor/huawei/alel04/BoardConfigVendor.mk
