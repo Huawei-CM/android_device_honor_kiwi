4b1d3331c29c1b1b9c5a5f23a62242fda19ef748
Ryan N. Norris <ryan.norris@my.ccsu.edu>
2015-09-13 20:49:34 -0400

    alel04: Fix init scripts again.

--- cm.mk
+++ cm.mk
@@ -8,13 +8,13 @@ PRODUCT_RELEASE_NAME := alel04
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/huawei/alel04/device.mk)
+$(call inherit-product, device/huawei/alel04/alel04.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := alel04
 PRODUCT_NAME := cm_alel04
 PRODUCT_BRAND := huawei
-PRODUCT_MODEL := P8 Lite
+PRODUCT_MODEL := ALE-L04
 PRODUCT_MANUFACTURER := Huawei
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
